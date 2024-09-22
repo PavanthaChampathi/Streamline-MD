ae27b2fe83a5ddaef57a414318930e23363b0188
Pavantha Champathi <pavanthachampathi@gmail.com>
2024-09-22 08:04:14 +0530

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,5 +1,10 @@
-const {cmd , commands} = require('../command')
+const fs = require('fs');
+const path = require('path');
 const config = require('../config');
+const { cmd, commands } = require('../command');
+
+// Path to images directory
+const IMG_PATH = path.join(__dirname, '../images');
 
 cmd({
     pattern: "alive",
@@ -10,6 +15,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     m.react("✨")
+    conn.sendMessage(from, image: { url: path.join(IMG_PATH, 'ALIVE_IMG.jpg') },, { quoted: mek });
 }catch(e){
 console.log(e)
 reply(`${e}`)
